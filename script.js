d7d7f0da493c4a84ae6fb933830f80e8acca7cf6
Fraxoo <fraxo.officiel@gmail.com>
2025-09-29 09:04:47 +0200

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const leftBlock = document.querySelector(".left-block")
+const rightBlockTemplate = document.getElementById("right-block");
 
 
 async function main() {
@@ -24,22 +25,43 @@ async function main() {
 
     const allPokemon = await getAllPokemon();
     console.log(allPokemon);
-    
+
     allPokemon.forEach(pokemon => {
         const templateList = document.getElementById("list");
         const cloneTemplateList = templateList.content.cloneNode(true);
         const leftId = cloneTemplateList.querySelector("#id");
         const leftName = cloneTemplateList.querySelector("#nom");
         const leftImg = cloneTemplateList.querySelector("#image")
-        console.log(leftName);
-        
+
         leftId.innerText = pokemon.id;
         leftName.innerText = pokemon.name;
-        leftImg.setAttribute("src",pokemon.image)
-        
+        leftImg.setAttribute("src", pokemon.image)
+
         leftBlock.appendChild(cloneTemplateList);
     });
+
+
+    const rightBlock = rightBlockTemplate.content.cloneNode(true);
+
+    const rightId = rightBlock.querySelector("#right-id");
+    const rightImg = rightBlock.querySelector("#right-img");
+    const rightNom = rightBlock.querySelector("h1");
+    const rightTypesDiv = rightBlock.querySelector(".types");
+
+    rightId.textContent = pokemonByID.id;
+    rightImg.setAttribute("src", pokemonByID.image);
+    rightNom.textContent = pokemonByID.name;
+    const pokemonTypes = pokemonByID.apiTypes;
+    pokemonTypes.forEach((type) => {
+        const typeImg = document.createElement("img")
+        typeImg.setAttribute("src",type.image);
+        rightTypesDiv.appendChild(typeImg);
+    })
     
+    
+    rightBlockTemplate.appendChild(rightBlock);
+
+
 
 
 
